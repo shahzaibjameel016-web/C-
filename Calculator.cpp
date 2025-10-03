1b0d567c76751c7c0950e9a6e27da9798957c3e0
shahzaibjameel016-web <shahzaibjameel016@gmail.com>
2025-10-03 10:54:38 +0500

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=0, b=0;
+    cout<< "Enter Two Numbers: ";
+    cin>>a >>b;
+    cout<<"Sum: " << a+b <<endl;
+    cout<<"Differernce: " << a-b<<endl ;
+    cout<<"Product: " << a*b <<endl;
+    cout<<"Quotient: " << a/b<<endl ;
+ 
+    return 0;
+}
